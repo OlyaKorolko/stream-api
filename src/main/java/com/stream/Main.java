cd1ec3fb722d4580c11f7bda1a3c35d39a2891fd
OlyaKorolko <71021414+OlyaKorolko@users.noreply.github.com>
2020-12-08 01:19:38 +0300

    Update Main.java

--- src/main/java/com/stream/Main.java
+++ src/main/java/com/stream/Main.java
@@ -2,9 +2,7 @@ package com.stream;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.LocalDate;
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
@@ -12,35 +10,60 @@ public class Main {
 
     public static void main(String[] args) {
         try (Stream<String> stringStream = Files.lines(Paths.get(".\\\\src\\\\data\\\\"))) {
-            List<Company> listOfCompanies = stringStream.map(line -> {
-                String[] x = line.split(",");
-                return new Company(x[0], x[1], LocalDate.parse(x[2]), x[3], LocalDate.parse(x[4]),
-                        Integer.parseInt(x[5]), x[6], x[7], x[8], x[9], x[10], x[11]);
-            }).collect(Collectors.toList());
-
-            Map<String, List<Company>> uniqueMap = listOfCompanies.stream().distinct()
-                    .filter(c -> c.getWebsiteAddress().length() <= 10)
-                    .collect(Collectors.groupingBy(Company::getAuditor, Collectors.toList()));
-
-            uniqueMap.forEach((key, value) -> System.out.println(key + " " + value));
-
-            List<Double> numbers = Arrays.asList(85.324, 4.345436, 21.54645, -324.1, -3214325.4, 0.343, 100.7777);
-
-            Double result = numbers.stream()
-                    .reduce((num1, num2) -> (num1 % num2) + Math.random() * 10)
-                    .orElse(0.0);
-
-            System.out.println("Result: " + result);
-            Double goldenNumber = numbers.stream()
-                    .filter(num -> Math.sqrt(Math.abs(num)) > 10)
-                    .findFirst()
-                    .orElse(-1.0);
-
-            System.out.println("Golden number: " + goldenNumber);
-            System.out.println("All of the elements match: " + numbers.stream().allMatch(goldenNumber::equals));
+            List<Company> listOfCompanies = Processor.writeToList(stringStream);
+            processRequest(listOfCompanies);
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
+    }
 
+    public static void printMenu() {
+        System.out.println("1. Limit by employee number bounds && group by auditors");
+        System.out.println("2. Find company by phone number pattern");
+        System.out.println("3. Find companies by location");
+        System.out.println("4. Find newest companies");
+    }
+
+    public static void processRequest(List<Company> listOfCompanies) {
+        try (Scanner scan = new Scanner(System.in)) {
+            while (true) {
+                printMenu();
+                switch (scan.nextInt()) {
+                    case 1:
+                        System.out.println("Enter the lower bound for employee number:");
+                        int lowerNumOfEmployees = scan.nextInt();
+                        System.out.println("Enter the upper bound for employee number:");
+                        int upperNumOfEmployees = scan.nextInt();
+                        Map<String, List<Company>> uniqueMap = Processor.filterByEmployeesGroupByAuditor(lowerNumOfEmployees,
+                                upperNumOfEmployees, listOfCompanies);
+                        System.out.println("Grouping by auditor.");
+                        uniqueMap.forEach((key, value) -> System.out.println(key + " " + value));
+                        break;
+                    case 2:
+                        System.out.println("1st company to have a 1-... phone number pattern:");
+                        Company company = Processor.findByPhoneNumber(listOfCompanies);
+                        System.out.println(company);
+                        break;
+                    case 3:
+                        System.out.println("Enter the location to see if any of the companies are located there: ");
+                        scan.nextLine();
+                        String location = scan.nextLine();
+                        if (Processor.findAnyCompanyByLocation(listOfCompanies, location)) {
+                            System.out.println("They do!");
+                        } else {
+                            System.out.println("We couldnt find any, your turn!");
+                        }
+                        break;
+                    case 4:
+                        System.out.println("Lets find n newest companies. Enter n:");
+                        int n = scan.nextInt();
+                        List<Company> newestCompanies = Processor.findNewestNCompanies(listOfCompanies, n);
+                        newestCompanies.forEach(System.out::println);
+                        break;
+                    default:
+                        return;
+                }
+            }
+        }
     }
 }
